1c8719915c0e5a51649f23c641a1e0a0bfa71dd8
mislavivanda <mislavivanda454@gmail.com>
2025-06-17 13:01:53 +0200

    Server update

--- homemate/next_build_output/server.js
+++ homemate/next_build_output/server.js
@@ -11,8 +11,52 @@ const handle = app.getRequestHandler();
 app.prepare().then(() => {
   const server = express();
 
-  server.use((req, res, nextMiddleware) => {
-    // Dynamically set assetPrefix for this request
+  // server.use((req, res, nextMiddleware) => {
+  //   // Dynamically set assetPrefix for this request
+  //   const referer = req.headers.referer || '';
+  //   const originalUri = req.headers['x-original-uri'] || '';
+  //   const ingressBase1 = req.url.match(
+  //     /^\/api\/hassio_ingress\/([a-zA-Z0-9-_]+)/
+  //   );
+  //   const ingressBase2 = referer.match(
+  //     /^\/api\/hassio_ingress\/([a-zA-Z0-9-_]+)/
+  //   );
+  //   const ingressBase3 = originalUri.match(
+  //     /^\/api\/hassio_ingress\/([a-zA-Z0-9-_]+)/
+  //   );
+  //   const ingressPathHeader = req.headers['x-ingress-path'];
+
+  //   const match = ingressPathHeader?.match(
+  //     /^\/api\/hassio_ingress\/[a-zA-Z0-9-_]+/
+  //   );
+  //   console.log(
+  //     'Request url',
+  //     req.url,
+  //     referer,
+  //     originalUri,
+  //     ingressBase1,
+  //     ingressBase2,
+  //     ingressBase3,
+  //     match
+  //   );
+  //   // if (ingressBase1) {
+  //   //   const basePath = `/api/hassio_ingress/${ingressBase1[1]}`;
+  //   //   app.setAssetPrefix(basePath);
+  //   // } else {
+  //   //   app.setAssetPrefix('');
+  //   // }
+  //   if (match && match[0]) {
+  //     const basePath = match[0];
+  //     console.log('🛠️ Setting assetPrefix to:', basePath);
+  //     app.setAssetPrefix(basePath);
+  //   } else {
+  //     app.setAssetPrefix('');
+  //   }
+  //   nextMiddleware();
+  // });
+
+  server.all('*', (req, res) => {
+        // Dynamically set assetPrefix for this request
     const referer = req.headers.referer || '';
     const originalUri = req.headers['x-original-uri'] || '';
     const ingressBase1 = req.url.match(
@@ -37,7 +81,8 @@ app.prepare().then(() => {
       ingressBase1,
       ingressBase2,
       ingressBase3,
-      match
+      match,
+      document.URL
     );
     // if (ingressBase1) {
     //   const basePath = `/api/hassio_ingress/${ingressBase1[1]}`;
@@ -52,10 +97,6 @@ app.prepare().then(() => {
     } else {
       app.setAssetPrefix('');
     }
-    nextMiddleware();
-  });
-
-  server.all('*', (req, res) => {
     return handle(req, res);
   });
 
